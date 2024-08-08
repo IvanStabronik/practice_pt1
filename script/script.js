5587307e993efa2834250b2668b34e681fde50d5
Ivan Stabronik <engel.stabronik@gmail.com>
2024-08-08 15:28:11 +0200

    second commit of practice UdemyPetrichenko

--- script/script.js
+++ script/script.js
@@ -10,12 +10,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("One of the last movies you saw?", ""),
-    b = prompt("How much would you rate it?", ""),
-    c = prompt("One of the last movies you saw?", ""),
-    d = prompt("How much would you rate it?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("One of the last movies you saw?", ""),
+        b = prompt("How much would you rate it?", "");
+
+    if(a != null && b !=null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Few movies seen");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('You\'re a classic viewer');
+} else if(personalMovieDB.count >= 30) {
+    console.log('You\'re a movie geek.');
+} else  {
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
+
+
+
